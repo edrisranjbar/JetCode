3eae290726497182f95e8e02089688e8254638ab
Edris Ranjbar <edris.qeshm2@gmail.com>
2021-02-21 02:46:15 +0330

    Blog category labels with colored shadow

--- assets/script.js
+++ assets/script.js
@@ -3,3 +3,15 @@ new Glider(document.querySelector('.glider'), {
     draggable: true,
     dots: '.dots',
 });
+
+// Categories shadow colors
+let colors = ["yellow", "blue", "black", "red", "green"]
+let categories = document.querySelectorAll(".blog-category");
+let i = 0;
+categories.forEach((element) => {
+    if (i >= 5) {
+        i = 0;
+    }
+    let color = colors[i++];
+    element.classList.add(`${color}-bg`);
+});
