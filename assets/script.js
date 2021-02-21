07cd8f3fbd8f44172642624521f3babd52d07d34
Edris Ranjbar <edris.qeshm2@gmail.com>
2021-02-21 05:54:58 +0330

    Toggle humberger menu plus transition added

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,30 @@
+let humberger_menu = document.querySelector('.humberger-menu');
+let humberger_menu_bars = document.querySelectorAll('.humberger-menu > .bar');
+let is_open = false;
 new Glider(document.querySelector('.glider'), {
     slidesToShow: "auto",
     draggable: true,
     dots: '.dots',
+});
+
+// Toggle Humberger menu
+function toggle_menu(is_open) {
+    if (!is_open) {
+        humberger_menu_bars.forEach((element) => {
+            element.style.display = "block";
+            element.style.transform = "unset";
+            element.style.top = "unset";
+        });
+    } else {
+        humberger_menu_bars[0].style.transform = "rotate(45deg)";
+        humberger_menu_bars[0].style.top = "11px";
+        humberger_menu_bars[1].style.display = "none";
+        humberger_menu_bars[2].style.transform = "rotate(-45deg)";
+    }
+}
+humberger_menu.addEventListener('click', function () {
+    toggle_menu(is_open);
+    is_open = !is_open;
 });
 
 // Categories shadow colors
