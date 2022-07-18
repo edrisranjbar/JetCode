7c67f5a2dd92c0411c39513191bebc33c8426883
edris <34417212+edrisranjbar@users.noreply.github.com>
2022-07-18 23:02:41 +0430

    adding splide to the project for sliders

--- footer.php
+++ footer.php
@@ -32,6 +32,7 @@
 </nav>
 
 </main>
+<script src="<?php echo get_template_directory_uri(); ?>/assets/js/splide.min.js"></script>
 <script src="<?php echo get_template_directory_uri(); ?>/assets/js/script.js"></script>
 </body>
 
